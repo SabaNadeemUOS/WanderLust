71fdcf1f08aa07d28287ccd7f1ce4ff1e3a40daa
sara AlHammadi <113388633+sarahalhammadi@users.noreply.github.com>
2022-11-20 22:36:43 +0400

    Update WanderLustJavaScript.js

--- JavaScript/WanderLustJavaScript.js
+++ JavaScript/WanderLustJavaScript.js
@@ -1,10 +1,42 @@
+// Get the button
+let mybutton = document.getElementById("myBtn");
 
-      // Get the button
-      let mybutton = document.getElementById("myBtn");
-      
-      // When the user clicks on the button, scroll to the top of the document
-      function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      }
-      
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//focus for the form
+
+var goidArray = ["Enter your name please",
+    "Enter you e-mail please",
+    "Rate us please",
+    "Enter your comment here please",
+    "Clck the button to submit the form",
+    "Click this button if you want to clear the form"
+];
+
+var goidText;
+
+function init() {
+    goidText = document.getElementById("goidText");
+
+    registerListenrs(document.getElementById("name"), 0);
+    registerListenrs(document.getElementById("email"), 1);
+    registerListenrs(document.getElementById("txtList"), 2);
+    registerListenrs(document.getElementById("comment"), 3);
+    registerListenrs(document.getElementById("submit"), 4);
+    registerListenrs(document.getElementById("clear"), 5);
+
+}
+
+function registerListenrs(object, messageNumber) {
+
+    object.addEventListener("focus",
+        function() { goidText.innerHTML = goidArray[messageNumber] }, false);
+
+    object.addEventListener("blur",
+        function() { goidText.innerHTML = goidArray[6] }, false);
+}
+window.addEventListener("load", init, false);
